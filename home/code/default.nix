fd546490167787f1fc2a5f1244cd587347740177
rdk31 <rdk31@protonmail.com>
2022-12-06 15:07:52 +0100

    update gitlens

--- home/code/default.nix
+++ home/code/default.nix
@@ -1,16 +1,28 @@
 { config, pkgs, lib, ... }:
 let
   marketplaceExtensions = [
+    #(pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+    #  mktplcRef = {
+    #    name = "copilot";
+    #    publisher = "GitHub";
+    #    version = "1.44.6735";
+    #    sha256 = "sha256-8Z16yfG4I6TpzzKUm6xLSEr6NT//pEjfW5+biC4G+4M=";
+    #  };
+
+    #  meta = {
+    #    license = lib.licenses.unfree;
+    #  };
+    #})
+
     (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
       mktplcRef = {
-        name = "copilot";
-        publisher = "GitHub";
-        version = "1.44.6735";
-        sha256 = "sha256-8Z16yfG4I6TpzzKUm6xLSEr6NT//pEjfW5+biC4G+4M=";
+        name = "gitlens";
+        publisher = "eamodio";
+        version = "2022.12.604";
+        sha256 = "sha256-yfqGITviASp5ZDEJA+zyVz1LpPWV4FM/4fU4eq52Xng=";
       };
-
       meta = {
-        license = lib.licenses.unfree;
+        license = lib.licenses.mit;
       };
     })
   ];
@@ -27,7 +39,7 @@ in
       #vadimcn.vscode-lldb 
       matklad.rust-analyzer
       vscodevim.vim
-      eamodio.gitlens
+      #eamodio.gitlens
       esbenp.prettier-vscode
       #formulahendry.auto-rename-tag
       #formulahendry.auto-close-tag
@@ -45,7 +57,7 @@ in
       james-yu.latex-workshop
       golang.go
       #yzhang.markdown-all-in-one
-      #github.copilot
+      github.copilot
     ]) ++ marketplaceExtensions;
   };
 
