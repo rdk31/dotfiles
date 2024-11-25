93d2e6ba531d268652d260c468233d1bef814207
rdk31 <rdk31@protonmail.com>
2024-11-25 02:31:16 +0100

    disable kittys bell

--- home/kitty.nix
+++ home/kitty.nix
@@ -1,10 +1,13 @@
-{ config, ... }:
+{ ... }:
 {
   programs.kitty = {
     enable = true;
     settings = {
       font_size = "14.0";
+      enable_audio_bell = false;
+      scrollback_lines = 100000;
     };
+    shellIntegration.enableZshIntegration = true;
   };
 
   home.sessionVariables = {
--- home/tmux.nix
+++ home/tmux.nix
@@ -6,6 +6,7 @@
     mouse = true;
     keyMode = "vi";
     customPaneNavigationAndResize = true;
+    historyLimit = 100000;
     extraConfig = ''
       bind -r Tab last-window
     '';
